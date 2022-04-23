b763c0e93133d75a1fc75f644512c540bdb95d83
Michael Zafanelli <maicomdassi@gmail.com>
2022-04-23 15:19:19 -0300

    Add Filter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import Filter from './components/Header/Filter/Index';
 
 function App() {
   return (
     <>
       <Header />
-      <div className="App-container"></div>
+      <div className="App-container">
+        <Filter />
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/Header/Filter/Index.tsx
@@ -0,0 +1,37 @@
+import './styles.css';
+import { Controller } from 'react-hook-form';
+import Select from 'react-select';
+
+function Filter() {
+  return (
+    <div className="filter-container base-card">
+      {/*       <div className="product-filter-category-conatiner">
+        <Controller
+          name="category"
+          control={control}
+          render={({ field }) => (
+            <Select
+              {...field}
+              options={selectCategories}
+              classNamePrefix="product-filter-select"
+              isClearable
+              placeholder="Categoria"
+              getOptionLabel={(category: Category) => category.name}
+              getOptionValue={(category: Category) => String(category.id)}
+              onChange={(value) => handleChangeCategory(value as Category)}
+            />
+          )}
+        />
+      </div> */}
+
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="MALE">Masculino</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="OTHER">Outro</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
